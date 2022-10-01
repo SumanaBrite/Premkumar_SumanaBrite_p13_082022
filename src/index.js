3bd407684e38faf14df50acccc6e0cced97abfde
Sumana <sumanabrite@gmail.com>
2022-10-01 15:52:28 +0200

    persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from '../src/store/store'
-// import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from '../src/store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
 import './index.css';
 
@@ -10,9 +10,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    {/* <PersistGate persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,52 @@
+// import { configureStore } from '@reduxjs/toolkit'
+
+// import { userReducer } from '../reducers/UserReducer'
+// import { loginReducer } from '../reducers/LoginReducer'
+
+// const store = configureStore({
+//   reducer: {
+//     userLogin: loginReducer,
+//     userProfile: userReducer,
+//   }
+// })
+// export default store
+
+import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 
+
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import ReduxThunk from 'redux-thunk'
 import { userReducer } from '../reducers/UserReducer'
 import { loginReducer } from '../reducers/LoginReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({
-  reducer: {
-    userLogin: loginReducer,
-    userProfile: userReducer,
-  }
+const persistConfig = {
+  key: 'persist-key',
+  storage,
+}
+const rootReducer = combineReducers({
+  userLogin: loginReducer,
+  userProfile: userReducer
 })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = configureStore(
+//   persistedReducer,
+//   composeWithDevTools(applyMiddleware(ReduxThunk))
+
+// )
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+
+})
+export const persistor = persistStore(store)
 export default store
+
+
+
+
+
+
